7b11a21163d1de47deeaed828930ef6c9bf46297
EloyOrtega <eortega@deustoseidor.com>
2023-01-11 21:08:09 +0100

    Create a feature component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 
 @Component({
@@ -8,8 +9,16 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
+  //selectedHero: Hero = {
+    hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+
+  selectedHero?: Hero;
+
+  heroes = HEROES;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
